2bf3c6c2a042a07026f20b00345c2afb6fb3d5f5
kirill <kiruha3@mail.ru>
2023-07-02 17:39:40 +0700

    firct commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int yearLeap = 1764;
+        int year = 1763;
+        leapYear(year);
+        leapYear(yearLeap);
+    }
+    private static void leapYear(int year) {
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println( year + " Год является високосным");
+        }else {
+            System.out.println( year + " Год является невисокосным");
+        }
     }
 }
